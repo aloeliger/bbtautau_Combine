6456a778ac60b4c05c6c975419429093ae50b690
Andrew David Loeliger <andrew.loeliger@cern.ch>
2022-01-04 21:03:45 +0100

    Update card creation script to process through to workspaces

--- combineHarvesterModel/scripts/createDataCards.py
+++ combineHarvesterModel/scripts/createDataCards.py
@@ -31,11 +31,9 @@ def main(args):
     #Now, log the command itself, so the process is recreatable
     os.system(cardCreationCommand + ' | tee -a ' + outputLoggingFileName)
     logging.info('Card Creation Command')
-    logging.info('\n\n'+cardCreationCommand)
+    logging.info('\n\n'+cardCreationCommand+'\n')
 
     #Now, process into a final text based datacard, and workable workspace
-    combinedCardName = 'FinalCard_'+theOutputArea.dateTag+'.txt'
-    cardCombiningCommand = 'combineCards.py'
 
     #let's find everything that looks like it is one of our cards
     #this isn't exactly perfect, but I don't have better ideas right now
@@ -47,13 +45,38 @@ def main(args):
         cardFile = open(cardName, "a+")
         cardFile.write("* autoMCStats 0.0\n")
         cardFile.close()
-        cardCombiningCommand += ' '+cardName
 
-    cardCombiningCommand += ' > ' + combinedCardName
-    #log it and do it
-    logging.info("Final Card Combining Command")
-    logging.info("\n\n"+cardCombiningCommand)
-    os.system(cardCombiningCommand+' |tee -a ' + outputLoggingFileName)
+    #okay, we need to group the cards up by mass point, because we have to do this mass point
+    #by mass point
+    #first thing to do, get a list of the available mass points
+    massPoints = []
+    for cardName in cardNames:
+        massPoint = re.search("[0-9]+(?=\.txt)",cardName).group(0)
+        if  massPoint not in massPoints:
+            massPoints.append(massPoint)
+
+    print("Mass Points in the analysis: ")
+    print(massPoints)
+
+    combinedCardNames = []
+    for massPoint in massPoints:
+        cardCombiningCommand = 'combineCards.py'
+        combinedCardName = 'FinalCard_'+theOutputArea.dateTag+'_'+massPoint+'.txt'
+        combinedCardNames.append(combinedCardName)
+        for cardName in cardNames:
+            if re.search('.+_'+massPoint+'\.txt',cardName):
+                cardCombiningCommand+= ' '+cardName+' '
+        cardCombiningCommand += ' > ' + combinedCardName
+        #log it and do it
+        logging.info("Mass Point {} Card Combining Command".format(massPoint))
+        logging.info("\n\n"+cardCombiningCommand)
+        os.system(cardCombiningCommand+' |tee -a ' + outputLoggingFileName)
+    
+        #Make a workspace? Or workspaces?
+        textToWorkspaceCommand = 'text2workspace.py -m '+massPoint+' '+combinedCardName
+        logging.info('Mass Point {} Text to workspace command: '.format(massPoint))
+        logging.info('\n\n'+textToWorkspaceCommand+'\n')
+        os.system(textToWorkspaceCommand+' | tee -a '+outputLoggingFileName)
 
     print("Finished!")
     theOutputArea.printOutputInfo()
